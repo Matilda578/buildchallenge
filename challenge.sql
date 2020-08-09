5e785ce6975657a5f2a49149de349e382d7df25d
Matilda578 <tilly.g@hotmail.com>
2020-08-10 08:52:18 +1000

    task 1 complete

---
+++ challenge.sql
@@ -0,0 +1,14 @@
+/*Matilda Goodison 101889190
+
+Event ( EventYear, EventMonth, EventDay,)
+PK (EventYear, EventMonth, EventDay)
+
+Tour ( TourName, Description)
+PK (TourName)
+
+Booking (DateBooked, Payment)
+PK (DateBooked)
+
+Client (ClientID, Surname, GivenName, Gender)
+PK (ClientID) */
+
